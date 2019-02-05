0de0c8371c1843c6a9c44c4225cf482f7d15c940
Samuel Hegner <C16439262@mydit.ie>
2019-02-05 16:59:16 +0000

    Some stuff

--- GE Semester 2/Assets/Boid.cs
+++ GE Semester 2/Assets/Boid.cs
@@ -21,7 +21,13 @@ public class Boid : MonoBehaviour
         
     }
 
-
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, force * 5);
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, velocity * 5);
+    }
 
     void Update()
     {
@@ -46,13 +52,32 @@ public class Boid : MonoBehaviour
     }
 
     Vector3 Seek(Vector3 target){
+
+        /*
+         * get the toTarget velocity
+         * normalise and multiply it by max speed (to target vector at max speed)
+         * calculate the desired velocity by taking away the current velocity
+        */
+
         Vector3 toTarget = target - transform.position;
         toTarget.Normalize();
-        toTarget *= maxSpeed;
-        return toTarget - velocity;
+        Vector3 desired = toTarget *= maxSpeed;
+        return desired - velocity;
     }
 
     Vector3 Arrive(Vector3 target){
+
+        /*
+         * generates a breaking force inside the slowing circle
+         * get the to target
+         * then get the distance   |T - B|
+         * distance/slowing distance * max speed (the closer you are to the target the slower you will get)
+         * outside the slowing circle the speed is too high therefore we clamp it
+         * minimum of the ramped speed and the max speed (.Min returns the smaller value)
+         * then normailse the clamped speed by * it by the totarget/distance
+         * the return the desired - the current velocity
+        */
+
         Vector3 toTarget = target - transform.position;
         float dist = toTarget.magnitude;
 
